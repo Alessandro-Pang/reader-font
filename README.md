79822ffcf73805c04e5801c3d7f4f21dd13349ad
Alessandro-pang <pcy1314@163.com>
2024-12-06 21:40:52 +0800

    fix: 修复 github pages 展示问题

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-# Vue 3 + TypeScript + Vite
+# Reader Font
 
-This template should help get you started developing with Vue 3 and TypeScript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+字体文件读取工具，用于读取字体文件中的内容并进行展示，可用于字体图标的单个字体的导出、下载等操作。
 
-Learn more about the recommended Project Setup and IDE Support in the [Vue Docs TypeScript Guide](https://vuejs.org/guide/typescript/overview.html#project-setup).
+## 使用方法
+
+### 安装
+
+```bash
+npm install
+```
+
+### 启动
+
+```bash
+npm run dev
+```
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+/*
+ * @Author: zi.yang
+ * @Date: 2024-12-05 09:23:18
+ * @LastEditors: zi.yang
+ * @LastEditTime: 2024-12-06 21:40:20
+ * @Description: 
+ * @FilePath: /reader-font/vite.config.ts
+ */
+import { defineConfig } from 'vite';
+
+import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig((conf) => {
+  const isProd = conf.mode === 'production';
+  return {
+    plugins: [vue()],
+    base: isProd ? '/reader-font/' : '/',
+  };
+});
